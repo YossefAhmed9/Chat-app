04208f0feafd28a5f21491032f7febef8b7918e5
Yossef Ahmed <yossefahmed209@gmail.com>
2023-11-16 18:33:09 +0200

    .

--- lib/Layouts/Register Screen.dart
+++ lib/Layouts/Register Screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 import '../components/components.dart';
 
@@ -14,8 +15,15 @@ class _Register_ScreenState extends State<Register_Screen> {
   var emailKey = GlobalKey<FormState>();
   var passKey = GlobalKey<FormState>();
   bool showpass = true;
-
+  bool isLoading = false;
   var passcontroller = TextEditingController();
+
+  Future<UserCredential> credintial() async {
+    return await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: emailcontroller.text,
+      password: passcontroller.text,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,148 +31,157 @@ class _Register_ScreenState extends State<Register_Screen> {
       appBar: AppBar(
         title: const Text('Register form'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: SingleChildScrollView(
-          child: Form(
-            key: formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  'Register',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w900,
-                    fontSize: 50.0,
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 50.0,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                defaultTextFormField(
-                  emailKey,
-                  TextInputType.emailAddress,
-                  emailcontroller,
-                  (value) {
-                    print(value);
-                  },
-                  (value) {
-                    print(value);
-                  },
-                  () {},
-                  'Email',
-                  const OutlineInputBorder(),
-                  const Icon(
-                    Icons.email_rounded,
+                  const SizedBox(
+                    height: 50,
                   ),
-                  (value) {
-                    if (value!.isEmpty) {
-                      return ('Email must be filled');
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: passcontroller,
-                  keyboardType: TextInputType.visiblePassword,
-                  onFieldSubmitted: (value) {
-                    print(value);
-                  },
-                  onChanged: (value) {
-                    print(value);
-                  },
-                  obscureText: showpass,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return ('Password must be filled');
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: const OutlineInputBorder(),
-                    prefixIcon: const Icon(Icons.lock_outline),
-                    suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            showpass = !showpass;
-                          });
-                        },
-                        child: const Icon(Icons.remove_red_eye_outlined)),
-                  ),
-                ),
-                const SizedBox(
-                  height: 35,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  height: 55.0,
-                  child: MaterialButton(
-                    onPressed: () async {
-                      if (formKey.currentState!.validate()) {
-                        print('email: ' + emailcontroller.text);
-                        print('pass: ' + passcontroller.text);
-                      }
-                      try {
-                        final credential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                          email: emailcontroller.text,
-                          password: passcontroller.text,
-                        );
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'weak-password') {
-                          print('The password provided is too weak.');
-                        } else if (e.code == 'email-already-in-use') {
-                          showSnackBar(
-                              context,
-                              'E-mail already in use, Go to login page and try to login with your account',
-                              5,
-                              Colors.deepPurpleAccent);
-                        }
-                      } catch (e) {
-                        print(e);
-                      }
+                  defaultTextFormField(
+                    emailKey,
+                    TextInputType.emailAddress,
+                    emailcontroller,
+                    (value) {
+                      print(value);
                     },
-                    color: Colors.blue,
-                    child: const Text(
-                      'Register',
-                      style: TextStyle(
-                        color: Colors.white,
+                    (value) {
+                      print(value);
+                    },
+                    () {},
+                    'Email',
+                    const OutlineInputBorder(),
+                    const Icon(
+                      Icons.email_rounded,
+                    ),
+                    (value) {
+                      if (value!.isEmpty) {
+                        return ('Email must be filled');
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: passcontroller,
+                    keyboardType: TextInputType.visiblePassword,
+                    onFieldSubmitted: (value) {
+                      print(value);
+                    },
+                    onChanged: (value) {
+                      print(value);
+                    },
+                    obscureText: showpass,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return ('Password must be filled');
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.lock_outline),
+                      suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              showpass = !showpass;
+                            });
+                          },
+                          child: const Icon(Icons.remove_red_eye_outlined)),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 55.0,
+                    child: MaterialButton(
+                      onPressed: () async {
+                        if (formKey.currentState!.validate()) {
+                          print('email: ' + emailcontroller.text);
+                          print('pass: ' + passcontroller.text);
+                        }
+                        try {
+                          isLoading = true;
+
+                          credintial().then((value) {
+                            showSnackBar(context, 'Email created successfully',
+                                5, Colors.teal);
+                            isLoading = false;
+                          });
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'weak-password') {
+                            showSnackBar(
+                                context,
+                                'The password provided is too weak.',
+                                5,
+                                Colors.red);
+                          } else if (e.code == 'email-already-in-use') {
+                            showSnackBar(
+                                context,
+                                'E-mail already in use, Go to login page and try to login with your account',
+                                5,
+                                Colors.deepPurpleAccent);
+                          }
+                        } catch (e) {
+                          print(e);
+                        }
+                      },
+                      color: Colors.blue,
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 35,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Already have an account?',
-                      style: TextStyle(
-                        fontSize: 18.0,
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Already have an account?',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
                       ),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text(
-                        'Login Now!',
-                        style: TextStyle(fontSize: 18),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          'Login Now!',
+                          style: TextStyle(fontSize: 18),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                // defaultButton(double.infinity, 50.0, Colors.red, () {
-                //   print('heeeeelo');
-                // }, 'Register', Colors.white, 18),
-              ],
+                    ],
+                  ),
+                  // defaultButton(double.infinity, 50.0, Colors.red, () {
+                  //   print('heeeeelo');
+                  // }, 'Register', Colors.white, 18),
+                ],
+              ),
             ),
           ),
         ),
